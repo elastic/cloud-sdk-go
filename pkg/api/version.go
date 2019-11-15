a16ffc80095d5aaf6e82d63f7ae834c4a1d68b33
Marc Lopez Rubio <marc5.12@outlook.com>
2019-11-15 17:46:21 +0800

    api: Add Version const defining ECE compatibility (#22)
    Adds an `api.Version` variable which can be used by clients to see which
    ECE compatibility version it has.
    Also changes the swagger target to automatically open their `${EDITOR}`
    after the models have been generated.
    
    ```
    $ make swagger
    [..]
    --> Please update "pkg/api/version.go" with the version number of the ECE compatible definition just used for code generation.
    -> Press enter to open the file with vim
    ```
    
    Signed-off-by: Marc Lopez <marc5.12@outlook.com>

---
+++ pkg/api/version.go
@@ -0,0 +1,27 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package api
+
+// Version contains the ECE API version compatibility for the current
+// auto-generated client and models. This needs to be updated every time a new
+// `api/apidocs.json` file added and the client and models are re-generated.
+// Even though the Bugfix version is specified, the general support statement is
+// on the minor version, i.e: 2.4.2 means that all the 2.4 branch is supported,
+// expecting some potentially unfixed bugs when ECE version is the same feature
+// version bug higher bugfix version.
+const Version = "2.4.2"
