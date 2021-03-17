0c5e6546cb82a912b3a6eb1518e59559967e06bc
Marc Lopez <marc5.12@outlook.com>
2021-03-17 17:58:17 +0100

    deploymentapi: Add OverrideCreateOrUpdateRequest
    Adds a new public function `OverrideCreateOrUpdateRequest` which allows
    setting certain overrides on the Deployment requests in the Create and
    Update operations.
    
    The function was private before making it part of the Create and Update
    calls. This wasn't always desirable specially when pairing it with the
    `--generate-payload` flag in `ecctl` where it wouldn't return the fields
    that were overridden.
    
    The function has been expanded to:
    
    - Set overrides to all the application `ref_id` fields.
    - ElasticsearchRefID overrides.
    - Plugin overrides.
    - Selection between node_roles and node_types when both are set.
    
    Signed-off-by: Marc Lopez <marc5.12@outlook.com>

--- pkg/api/deploymentapi/override_settings.go
+++ pkg/api/deploymentapi/override_settings.go
@@ -17,7 +17,13 @@
 
 package deploymentapi
 
-import "github.com/elastic/cloud-sdk-go/pkg/models"
+import (
+	"github.com/blang/semver/v4"
+	"github.com/elastic/cloud-sdk-go/pkg/models"
+	"github.com/elastic/cloud-sdk-go/pkg/util/ec"
+)
+
+var dataTiersMinVersion = semver.MustParse("7.10.0")
 
 // PayloadOverrides represent the override settings to
 type PayloadOverrides struct {
@@ -26,21 +32,30 @@ type PayloadOverrides struct {
 
 	// If set, it will override the region when not present in the
 	// DeploymentCreateRequest.
-	// Note this behaviour is different from the rest of overrides
+	// Note this behavior is different from the rest of overrides
 	// since this field tends to be populated by the global region
 	// field which is implicit (by config) rather than explicit by flag.
 	Region string
 
 	// If set, it'll override all versions to match this one.
 	Version string
+
+	// ElasticsearchRefID used for the applications.
+	ElasticcsearchRefID string
+
+	// OverrideRefIDs when set, it'll override all the application's ref_id.
+	OverrideRefIDs bool
+
+	// Slice of built-in enabled plugins.
+	ElasticcsearchBuiltinPlugins []string
 }
 
-// setOverrides sets a series of overrides to either the Create or Update
+// OverrideCreateOrUpdateRequest sets a series of overrides to either the Create or Update
 // deployment request. See PayloadOverrides to understand which how each
 // field of that struct affects the behavior of this function.
-func setOverrides(req interface{}, overrides *PayloadOverrides) {
+func OverrideCreateOrUpdateRequest(req interface{}, overrides *PayloadOverrides) error {
 	if req == nil || overrides == nil {
-		return
+		return nil
 	}
 
 	var apm []*models.ApmPayload
@@ -51,7 +66,7 @@ func setOverrides(req interface{}, overrides *PayloadOverrides) {
 	switch t := req.(type) {
 	case *models.DeploymentUpdateRequest:
 		if t.Resources == nil {
-			return
+			return nil
 		}
 		apm, appsearch = t.Resources.Apm, t.Resources.Appsearch
 		elasticsearch, kibana = t.Resources.Elasticsearch, t.Resources.Kibana
@@ -61,25 +76,36 @@ func setOverrides(req interface{}, overrides *PayloadOverrides) {
 			t.Name = overrides.Name
 		}
 		if t.Resources == nil {
-			return
+			return nil
 		}
 		apm, appsearch = t.Resources.Apm, t.Resources.Appsearch
 		elasticsearch, kibana = t.Resources.Elasticsearch, t.Resources.Kibana
 		enterprisesearch = t.Resources.EnterpriseSearch
 	}
 
-	overrideByPayload(
+	return overrideByPayload(
 		apm, appsearch, elasticsearch, kibana, enterprisesearch,
-		overrides.Region, overrides.Version,
+		overrides.Region, overrides.Version, overrides.ElasticcsearchRefID,
+		overrides.ElasticcsearchBuiltinPlugins, overrides.OverrideRefIDs,
 	)
 }
 
 // nolint
 func overrideByPayload(apm []*models.ApmPayload, appsearch []*models.AppSearchPayload,
-	elasticsearch []*models.ElasticsearchPayload, kibana []*models.KibanaPayload, enterprisesearch []*models.EnterpriseSearchPayload, region, version string) {
+	elasticsearch []*models.ElasticsearchPayload, kibana []*models.KibanaPayload,
+	enterprisesearch []*models.EnterpriseSearchPayload, region, version, refID string,
+	plugins []string, overrideRefIDs bool) error {
 	for _, resource := range apm {
 		if resource.Region == nil && region != "" {
 			resource.Region = &region
+		}
+
+		if overrideRefIDs {
+			resource.RefID = ec.String("main-apm")
+		}
+
+		if refID != "" {
+			resource.ElasticsearchClusterRefID = &refID
 		}
 
 		if version != "" {
@@ -94,6 +120,14 @@ func overrideByPayload(apm []*models.ApmPayload, appsearch []*models.AppSearchPa
 			resource.Region = &region
 		}
 
+		if overrideRefIDs {
+			resource.RefID = ec.String("main-appsearch")
+		}
+
+		if refID != "" {
+			resource.ElasticsearchClusterRefID = &refID
+		}
+
 		if version != "" {
 			if resource.Plan != nil && resource.Plan.Appsearch != nil {
 				resource.Plan.Appsearch.Version = version
@@ -106,9 +140,35 @@ func overrideByPayload(apm []*models.ApmPayload, appsearch []*models.AppSearchPa
 			resource.Region = &region
 		}
 
+		if refID != "" {
+			resource.RefID = &refID
+		}
+
+		// If version is empty, then data tiers are supported by default.
+		dataTierCompatible := true
 		if version != "" {
 			if resource.Plan != nil && resource.Plan.Elasticsearch != nil {
 				resource.Plan.Elasticsearch.Version = version
+				if len(plugins) > 0 {
+					resource.Plan.Elasticsearch.EnabledBuiltInPlugins = append(
+						resource.Plan.Elasticsearch.EnabledBuiltInPlugins, plugins...,
+					)
+				}
+
+				esVersion, err := semver.Parse(version)
+				if err != nil {
+					return err
+				}
+				dataTierCompatible = esVersion.GE(dataTiersMinVersion)
+			}
+		}
+		for _, top := range resource.Plan.ClusterTopology {
+			if len(top.NodeRoles) > 0 && top.NodeType != nil {
+				if dataTierCompatible {
+					top.NodeType = nil
+					continue
+				}
+				top.NodeRoles = nil
 			}
 		}
 	}
@@ -116,6 +176,14 @@ func overrideByPayload(apm []*models.ApmPayload, appsearch []*models.AppSearchPa
 	for _, resource := range enterprisesearch {
 		if resource.Region == nil && region != "" {
 			resource.Region = &region
+		}
+
+		if overrideRefIDs {
+			resource.RefID = ec.String("main-enterprise_search")
+		}
+
+		if refID != "" {
+			resource.ElasticsearchClusterRefID = &refID
 		}
 
 		if version != "" {
@@ -130,10 +198,20 @@ func overrideByPayload(apm []*models.ApmPayload, appsearch []*models.AppSearchPa
 			resource.Region = &region
 		}
 
+		if overrideRefIDs {
+			resource.RefID = ec.String("main-kibana")
+		}
+
+		if refID != "" {
+			resource.ElasticsearchClusterRefID = &refID
+		}
+
 		if version != "" {
 			if resource.Plan != nil && resource.Plan.Kibana != nil {
 				resource.Plan.Kibana.Version = version
 			}
 		}
 	}
+
+	return nil
 }
