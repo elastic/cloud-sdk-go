09020f8a52ea17c07804ae7a57ed3294a51f9271
Patroklos Papapetrou <pat@elastic.co>
2021-03-17 19:20:27 +0200

    implements the comment get api operation

---
+++ pkg/api/commentapi/get_params.go
@@ -0,0 +1,57 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package commentapi
+
+import (
+	"errors"
+
+	"github.com/elastic/cloud-sdk-go/pkg/api"
+	"github.com/elastic/cloud-sdk-go/pkg/api/apierror"
+	"github.com/elastic/cloud-sdk-go/pkg/multierror"
+	"github.com/elastic/cloud-sdk-go/pkg/util/ec"
+)
+
+// GetParams is consumed by the Get function.
+type GetParams struct {
+	*api.API
+	ResourceType, ResourceID, CommentID, Region string
+}
+
+// Validate ensures the parameters are usable by Get.
+func (params GetParams) Validate() error {
+	errs := multierror.NewPrefixed("invalid comment get params",
+		ec.RequireRegionSet(params.Region),
+	)
+
+	if params.API == nil {
+		errs = errs.Append(apierror.ErrMissingAPI)
+	}
+
+	if params.ResourceType == "" {
+		errs = errs.Append(errors.New("resource type is required"))
+	}
+
+	if params.ResourceID == "" {
+		errs = errs.Append(errors.New("resource id is required"))
+	}
+
+	if params.CommentID == "" {
+		errs = errs.Append(errors.New("comment id is required"))
+	}
+	return errs.ErrorOrNil()
+}
---
+++ pkg/api/commentapi/get_params_test.go
@@ -0,0 +1,71 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package commentapi
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/elastic/cloud-sdk-go/pkg/api"
+	"github.com/elastic/cloud-sdk-go/pkg/multierror"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetParams_Validate(t *testing.T) {
+	tests := []struct {
+		name   string
+		params GetParams
+		err    error
+	}{
+		{
+			name:   "should return all validation errors",
+			params: GetParams{},
+			err: multierror.NewPrefixed("invalid comment get params",
+				errors.New("region not specified and is required for this operation"),
+				errors.New("api reference is required for the operation"),
+				errors.New("resource type is required"),
+				errors.New("resource id is required"),
+				errors.New("comment id is required"),
+			),
+		},
+		{
+			name: "should return no validation errors",
+			params: GetParams{
+				API:          &api.API{},
+				ResourceType: "some-type",
+				ResourceID:   "some-id",
+				CommentID:    "some-comment-id",
+				Region:       "some-region",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := tt.params.Validate()
+			if err != nil && tt.err == nil {
+				t.Fatalf("expected no errors but got %+v", err)
+			}
+			if err == nil && tt.err != nil {
+				t.Fatalf("expected errors %+v but got no errors", tt.err)
+			}
+			if err != nil && tt.err != nil {
+				assert.EqualError(t, err, tt.err.Error())
+			}
+		})
+	}
+}
