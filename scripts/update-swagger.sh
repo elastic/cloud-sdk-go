60edb2fdad3f9dbe73b7ef3f944b320c6ac8a11e
Marc Lopez Rubio <marc5.12@outlook.com>
2021-08-20 15:23:35 +0300

    swagger: Script the OpenAPI spec update (#342)
    Adds a new script that updates the OpenAPI spec with the milestone of
    the ECE release.
    
    Signed-off-by: Marc Lopez Rubio <marc5.12@outlook.com>

---
+++ scripts/update-swagger.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+SDK_PATH=$(pwd)
+BRANCH=${1}
+SWAGGER_DEF_PATH=scala-services/adminconsole/src/main/resources
+GIT_CLOUD_REPO=https://github.com/elastic/cloud
+REPO_PATH=${2}
+
+if [[ -d ${REPO_PATH} ]]; then
+    cd ${REPO_PATH}
+    git fetch
+    git checkout ${BRANCH}
+    git pull
+else
+    git clone --single-branch -b ${BRANCH} ${GIT_CLOUD_REPO} ${REPO_PATH}
+fi
+
+cp ${REPO_PATH}/${SWAGGER_DEF_PATH}/apidocs.json ${SDK_PATH}/api/
+cp ${REPO_PATH}/${SWAGGER_DEF_PATH}/apidocs-user.json ${SDK_PATH}/api/
